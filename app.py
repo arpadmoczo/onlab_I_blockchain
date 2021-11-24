cbbb449285950dc11d3a9bc1bb2dc2688031e451
arpadmoczo <58360514+arpadmoczo@users.noreply.github.com>
2021-11-24 12:03:59 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+from blockchain.scripts.deploy_lottery import deploy_lottery, start_lottery, enter_lottery, end_lottery
+
+app = Flask(__name__)
+
+@app.route('/user', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        account = request.form.get('account')
+        usdEntryFee = request.form.get('usdEntryFee')
+        print(account)
+        #deploy_lottery()
+        #enter_lottery(account=account, value=usdEntryFee)
+
+    return render_template('index.html') 
+
+@app.route('/admin')
+def admin_start():
+    if request.method == 'POST':
+        #deploy_lottery()
+        #start_lottery()
+        pass
+
+    return render_template('admin.html')
+
+@app.route('/admin')
+def admin_end():
+    if request.method == 'POST':
+        #deploy_lottery()
+        #winner = end_lottery()
+        #return render_template('admin.html', winner=winner)
+        pass
+
+    return render_template('admin.html')
+
+if __name__ == '__app__':
+    app.run(debug=True)
