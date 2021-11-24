132de4bac60309c655f138211caf95460c316dc8
arpadmoczo <58360514+arpadmoczo@users.noreply.github.com>
2021-11-24 12:01:34 +0100

    Add files via upload

---
+++ blockchain/scripts/deploy_mocks.py
@@ -0,0 +1,7 @@
+#Based on a FreeCodeCamp video. Source: https://www.youtube.com/watch?v=M576WGiDBdQ&ab_channel=freeCodeCamp.org
+
+from scripts.helpful_scripts import deploy_mocks
+
+
+def main():
+    deploy_mocks()
